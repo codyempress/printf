f5c98430b8ab855699fc0d14594984e3b8f3605e
charlie-shadow-walker <mastercharlie1998@gmail.com>
2022-11-24 10:57:28 -0800

    _printf function

--- _printf.c
+++ _printf.c
@@ -1,32 +1,56 @@
-#include "main.h"
 #include <stdarg.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 /**
- * _printf - prints output according to a format
- * @format: string
+ * _printf - produces output according to a format
+ * @format: character string
  *
- * Return: number of characters printed
+ * Return:  the number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	va_list list;
-	int i = 0;
-
-	va_start(list, format);
-
-	while (format[i] != '\0')
-	{
-		if (format[i] == 'c')
-		{
-			_putchar (va_arg(list, int));
-			return (1);
-		}
-		else if (format[i] == 's')
-		{
-			printf("%s", va_arg(list, char *));
-			return (strlen(va_arg(list, char *)));
-		}
-	}
+        va_list list;
+        int i = 0;
+        va_start(list, format);
+        for (i= 0; format[i] != '\0'; i++)
+        {
+            if (format[i] == '%')
+        {
+                if (format[i + 1] == 'c')
+                {
+                        printf("%c\n", va_arg(list, int));
+                        return (1);
+                }
+                else if (format[i + 1] == 's')
+                {
+                        printf("%s\n", va_arg(list, char *));
+                        return (strlen(va_arg(list, char *)));
+                }
+                else if (format[i + 1] == 'i')
+                {
+                        printf ("%d\n", va_arg(list, int));
+                        return (sizeof(va_arg(list, int)) / sizeof(int));
+                }
+                else if (format[i + 1] == 'd')
+                {
+                    printf("%d\n", va_arg(list, int));
+                    return(sizeof(va_arg(list, int) / sizeof(int)));
+                }
+                else if (format[i + 1] == 'u')
+                {
+                        printf("%u\n", va_arg(list, unsigned int));
+                        return (sizeof(va_arg(list, unsigned int) / sizeof(unsigned int)));
+                }
+                else if (format[i + 1] == 'o')
+                {
+                    printf("%o\n", va_arg(list, int));
+                    return(sizeof(va_arg(list, int)) / sizeof(unsigned int));
+                }
+                else if (format[i + 1] == 'p')
+                {
+                    printf("%p\n", va_arg(list, char *));
+                    return(strlen(va_arg(list, char *)));
+                }
+        }
+        va_end(list);
 }
